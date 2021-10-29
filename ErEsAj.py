87b3fd83b7735dcb2b1da283227edd27051bba4b
mattibijnens <matti.bijnens@hotmail.com>
2021-10-29 10:13:50 +0200

    Fixing obviously broken keys
    Who put these there in the first place, I wonder

--- ErEsAj.py
+++ ErEsAj.py
@@ -2,12 +2,12 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
-password = b'thepassword'
+password = b'qmlsdkfjoaqijef'
 
 key = rsa.generate_private_key(
     backend=default_backend(),
     public_exponent=65537,
-    key_size=2048
+    key_size=4096
 )
 
 private_key = key.private_bytes(
@@ -23,12 +23,12 @@ public_key = key.public_key().public_bytes(
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
-password = b'thepassword'
+password = b'qsdfqsdf'
 
 key = rsa.generate_private_key(
     backend=default_backend(),
     public_exponent=65537,
-    key_size=2048
+    key_size=4096
 )
 
 private_key = key.private_bytes(
